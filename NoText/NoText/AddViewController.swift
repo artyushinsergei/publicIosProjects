2b16d8092c483ede069c5aad32bed8725da39abb
artyushinsergei <artserg19@gmail.com>
2022-04-11 09:38:39 +0300

    UIkit Notext, Bithday traker
    Частично переписал окно с добавлением заметки с UIKit.
    
    Накидал дизайн для Bithday Tracker

--- NoText/NoText/AddViewController.swift
+++ NoText/NoText/AddViewController.swift
@@ -7,32 +7,89 @@
 
 import UIKit
 
+
 class AddViewController: UIViewController {
 
-    //MARK: IBOutlet
-    @IBOutlet weak var titleText: UITextField!
-    @IBOutlet weak var noteText: UITextField!
-    
-    @IBOutlet weak var saveButton: UIButton!
-    
+    var saveButton = UIButton()
+    var titleText = UITextField()
+    var titleLable = UILabel()
+    var mainTextLable = UILabel()
+    var mainText = UITextView
+        
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.title = "Note form"
         
+        mainTextLableFunc()
         saveButtonFunc()
+        titleTextFieldFunc()
+        titleLebleFunc()
     }
     
+
+    //MARK: Тайтл Лабл
+    func titleLebleFunc(){
+        titleLable.frame = CGRect(x: 25, y: 120, width: 350, height: 25)
+        titleLable.text = "Note title"
+        titleLable.font = UIFont.boldSystemFont(ofSize: 30)
+        //titleLable.font.withSize(30.0)
+        
+        titleLable.textAlignment = .left
+        titleLable.textColor = UIColor.green
+        //Выровнять по середине
+        view.addSubview(titleLable)
+        
+    }
+
+    //MARK: Поле ввода заголовка
+    func titleTextFieldFunc(){
+        titleText.frame = CGRect(x: 12, y: 155, width: 350, height: 28)
+        titleText.placeholder = " Enter the title"
+        titleText.textColor = UIColor.green
+        titleText.font = UIFont.boldSystemFont(ofSize: 20)
+        titleText.layer.cornerRadius = 5
+        titleText.layer.borderWidth = 1
+        titleText.layer.borderColor = CIColor.magenta
+        //titleText.layer.borderColor = UIColor.lightGray.cgColor
+        
+        view.addSubview(titleText)
+        
+    }
+    
+    //MARK: Мэин Лабл
+    func mainTextLableFunc(){
+        mainTextLable.frame = CGRect(x: 25, y: 195, width: 350, height: 28)
+        mainTextLable.text = "Main text"
+        mainTextLable.font = UIFont.boldSystemFont(ofSize: 30)
+        mainTextLable.textAlignment = .left
+        mainTextLable.textColor = UIColor.green
+        //Выровнять по середине
+        view.addSubview(mainTextLable)
+        
+    }
+    
+    func mainTextFunc(){
+        
+    }
+    
+    //MARK: Кнопка сохранить
     func saveButtonFunc(){
-        
-        if ((titleText.text?.isEmpty) != nil){
-            saveButton.isHidden = true
-        }
-        
+        saveButton.frame = CGRect(x: 12, y: 665 , width: 350, height: 45)
+        saveButton.setTitle("Save", for: .normal)
+        saveButton.setTitleColor(.green, for: .normal)
+        saveButton.tintColor = UIColor.green
         saveButton.backgroundColor = .clear
         saveButton.layer.cornerRadius = 5
         saveButton.layer.borderWidth = 1
         saveButton.layer.borderColor = UIColor.green.cgColor
+        saveButton.addTarget(self, action: #selector(onClickSaveButton(paramSender:)), for: .touchUpInside)
+        view.addSubview(saveButton)
     }
     
+    //MARK: Действие по нажатию кнопки
+    @objc func onClickSaveButton(paramSender: Any){
+        print("Button is pressed")
+    }
     
     
     //MARK: Actions
