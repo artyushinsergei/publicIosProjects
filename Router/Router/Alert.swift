8e569557f7612ea18d16153d30ea6d27f78eb170
artyushinsergei <artserg19@gmail.com>
2022-05-26 09:04:56 +0300

    add Router Project
    -Начал писать проект на построение маршрута по точкам
    -Прописал основной интерфейс
    -Создал алерт на добавление адреса и алерт на ошибку

---
+++ Router/Router/Alert.swift
@@ -0,0 +1,37 @@
+//
+//  Alert.swift
+//  Router
+//
+//  Created by SERGEI on 26.05.2022.
+//
+
+import UIKit
+
+extension UIViewController{
+    func alertController(title: String,placeHolde: String, complitionHandler: @escaping (String) -> Void){
+        let alertController = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+        let alertOk = UIAlertAction(title: "Ok", style: .default, handler: {(action) in
+            let tfText = alertController.textFields?.first
+            guard let text = tfText?.text else {return}
+            complitionHandler(text)
+        })
+        
+        alertController.addTextField { (tf) in
+            tf.placeholder = placeHolde
+        }
+        
+        let alertCancel = UIAlertAction(title: "Cancel", style: .default, handler: {(_) in })
+        
+        alertController.addAction(alertOk)
+        alertController.addAction(alertCancel)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func alertError(title: String, message: String){
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertOk = UIAlertAction(title: "ok", style: .default)
+        alertController.addAction(alertOk)
+        present(alertController, animated: true, completion: nil)
+    }
+}
---
+++ Router/Router/ViewController.swift
@@ -0,0 +1,104 @@
+//
+//  ViewController.swift
+//  Router
+//
+//  Created by SERGEI on 26.05.2022.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController {
+    
+    let mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        return mapView
+    }()
+    
+    @objc let addAddressButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Add Address", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+
+        return button
+    }()
+    
+    let routeButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Route", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.isHidden = true 
+        return button
+    }()
+    
+    let resetButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Reset", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.isHidden = true
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setConstrains()
+        
+        addAddressButton.addTarget(self, action: #selector(addAddressButtonTapped), for: .touchUpInside)
+        routeButton.addTarget(self, action: #selector(routeButtonTapped), for: .touchUpInside)
+        resetButton.addTarget(self, action: #selector(resetButtonTapped), for: .touchUpInside)
+        
+    }
+    
+    @objc func addAddressButtonTapped(){
+        alertController(title: "Add", placeHolde: "Enter ur address") { (text) in
+            print(text)
+        }
+    }
+    
+    @objc func routeButtonTapped(){
+        print("route")
+    }
+    
+    @objc func resetButtonTapped(){
+        print("Reset")
+    }
+}
+
+extension ViewController{
+    func setConstrains(){
+        view.addSubview(mapView)
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor, constant:0),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant:0),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant:0),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant:0)
+        ])
+        
+        view.addSubview(addAddressButton)
+        NSLayoutConstraint.activate([
+            addAddressButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 50),
+            addAddressButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            addAddressButton.heightAnchor.constraint(equalToConstant: 70),
+            addAddressButton.widthAnchor.constraint(equalToConstant: 120)
+            ])
+        
+        view.addSubview(resetButton)
+        NSLayoutConstraint.activate([
+            resetButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -30),
+            resetButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            resetButton.heightAnchor.constraint(equalToConstant: 70),
+            resetButton.widthAnchor.constraint(equalToConstant: 120)
+            ])
+        
+        view.addSubview(routeButton)
+        NSLayoutConstraint.activate([
+            routeButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            routeButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -30),
+            routeButton.heightAnchor.constraint(equalToConstant: 70),
+            routeButton.widthAnchor.constraint(equalToConstant: 120)
+            ])
+    }
+}
